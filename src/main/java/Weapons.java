684120b88aedb509b66cbcf1330ffd4cbf11b8b2
EnigmaticGale <hamishwhytehw@gmail.com>
2019-04-04 12:49:10 +0100

    added basic enums

---
+++ src/main/java/Weapons.java
@@ -0,0 +1,18 @@
+public enum Weapons {
+
+    SWORD(10),
+    AXE(15),
+    MACE(20);
+
+    private final int damage;
+
+
+
+    Weapons(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+}
