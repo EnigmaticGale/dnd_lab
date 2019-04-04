0849a977363659dc319caccc7c56d9a5e32a93e7
EnigmaticGale <hamishwhytehw@gmail.com>
2019-04-04 13:01:53 +0100

    finished basic weapon,spell,healthitem enums

--- src/main/java/HealingItems.java
+++ src/main/java/HealingItems.java
@@ -1,4 +1,18 @@
-public class HealingItems {
+public enum HealingItems {
+
+    POTION(30),
+    HERBS(10),
+    LAYONHANDS(40);
+
+    private final int health;
+
+    HealingItems(int health) {
+        this.health = health;
+    }
+
+    public int getHealth() {
+        return health;
+    }
 
 
 }
--- src/main/java/Spells.java
+++ src/main/java/Spells.java
@@ -2,5 +2,16 @@ public enum Spells {
 
     FIREBALL(20),
     ICEBLAST(20),
+    MAGICMISSILE(30);
+
+    private final int damage;
+
+    Spells(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
 
 }
